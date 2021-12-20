dd4e0345361ce5913a6911d4759b84c2dc0d3410
thenhao <94664145+thenhao@users.noreply.github.com>
2021-12-20 17:11:06 +0800

    Added millisecond section and also checking for colour change. Added default conditions and also padding for the timing display. Added styles.css to the layout of the page

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 let customSetTime = 0;
 //Variable for minute, second and millisecond time in seconds
 let minuteTime = 0;
-let secondTime =0;
+let secondTime = 0;
+let millisecondTime = 100;
 let minuteTimeOverall = 0;
 let secondTimeOverall = 0;
 let minutePassed = 0;
@@ -12,17 +13,32 @@ let secondHand =0;
 
 //default time for minute and second
 const defaultMinute = 4;
-const defaultSecond =0;
+const defaultSecond = 0;
+const defaultMilisecond = 0;
+const defaultIntervalCounter = 30;
 //variable for new line
 let newLine ="\n";
 //array for number of 30 second block
 let timeBlockArray;
-let counter =0;
+let counter = 0;
 let currentCount = 0;
 //variable for set interval
 let startTimeCounter;
+let startMilliTimeCounter;
 //variable for button clicked or not
 let isClicked = false;
+let started = false;
+
+
+document.getElementById("min").innerText = padZero(defaultMinute);
+document.getElementById("sec").innerText = padZero(defaultSecond);
+document.getElementById("mili").innerText = padZero(defaultMilisecond);
+
+document.getElementById("formin").innerText = padZero(defaultSecond);
+document.getElementById("forsec").innerText = padZero(defaultIntervalCounter);
+document.getElementById("formili").innerText = padZero(defaultMilisecond);
+
+resetTime();
 
 function calculateSetTime(){
     //console.log to troubleshoot showing can get value in
@@ -32,11 +48,11 @@ function calculateSetTime(){
     initialTime();
 
     //set the string in place
-     document.getElementById("timeDisplay").innerText = minuteTime/60 +"min"+secondTime+"seconds";
     
      calculateBlocksOfTime();
 
-     document.getElementById("timeDisplay").innerText = (minuteHand) +"min"+ (secondHand) +" seconds" +newLine+timeBlockArray[counter];
+
+
 }
 
 function initialTime(){
@@ -76,14 +92,44 @@ function calculateBlocksOfTime(){
 function startTime(){
     //isClicked variable to check if the button is clicked
     isClicked = true;
+    //another timer for millisecond ? // for nice looking purpose
+    startMilliTimeCounter = setInterval(millisecondShow,10);
     startTimeCounter = setInterval(tabathaCount,1000);
+}
+
+function millisecondShow(){
+    //default value to countdown
+    //default value to show as something else
+    //hit 0 second time minus// however let the other timer handle
+    if(millisecondTime === -1){
+        millisecondTime = 100;
+    }
+    else if(millisecondTime === 100){
+        millisecondTime--;
+    }
+
+    //tested with a label and it works.
+    
+    if(millisecondTime === 100){
+        document.getElementById("mili").innerText = padZero(defaultMilisecond);
+        document.getElementById("formili").innerText = padZero(defaultMilisecond);
+    }
+    else{
+        document.getElementById("mili").innerText = padZero(millisecondTime);
+        document.getElementById("formili").innerText = padZero(millisecondTime);
+    }
+    
+    
+    //console.log(millisecondTime);
+    millisecondTime--;
 }
 
 function tabathaCount(){   
     if(timeBlockArray[counter] === 0){
         
         if(counter === 0){
-            document.getElementById("timeDisplay").innerText = timeBlockArray[counter];
+            //document.getElementById("timeDisplay").innerText = timeBlockArray[counter];
+            document.getElementById("forsec").innerText =padZero(defaultSecond);
             clearInterval(startTimeCounter);
         }else{
             counter--;  
@@ -100,42 +146,6 @@ function tabathaCount(){
     //button clicked or not
     if(isClicked)
     {
-        // if(minutePassed === 2 || minutePassed === 0){
-        //     secondTimeOverall = timeBlockArray[counter] + timeBlockArray[counter-1];
-        //     console.log(secondTimeOverall);
-        // }else if(minutePassed === 1){
-        //     secondTimeOverall = timeBlockArray[counter];
-        //     console.log(secondTimeOverall);
-        //     //console.log(counter);
-        // }
-        
-        // if(secondTimeOverall === 60){
-            
-            
-        //     //console.log('in here');
-        //     // if((minuteTimeOverall) !== 0){
-
-                
-                
-        //     // }else{
-        //     //     clearInterval(startTimeCounter);
-        //     // }
-            
-        // }else if(secondTimeOverall === 0){
-        //     if(minutePassed ===0){
-        //         minutePassed = 2;
-        //     }else{
-        //         minutePassed--;
-        //     }
-            
-        // }else{
-
-        //     if(secondTimeOverall === 59){
-        //         minuteTimeOverall--;
-        //     }
-        //     document.getElementById("timeDisplay").innerText = (minuteTimeOverall) +"min"+(secondTimeOverall)+"seconds" +newLine+timeBlockArray[counter];
-        // }
-
         //This if condition checks if 1 minute has passed
         if(minuteHand !== 0){
             if(minutePassed === 60){
@@ -154,7 +164,10 @@ function tabathaCount(){
         if(secondHand === 0){
             if(minuteHand === 0){
                 //end as minute =0 and seconds = 0
+                document.getElementById("mili").innerText = padZero(defaultMilisecond);
+                document.getElementById("formili").innerText = padZero(defaultMilisecond);
                 clearInterval(startTimeCounter);
+                clearInterval(startMilliTimeCounter);
             }
             else{
                 minuteHand--;
@@ -165,20 +178,38 @@ function tabathaCount(){
             minuteHand--;
             secondHand--;
             timeBlockArray[counter]--;
+            
         }
 
         //This section displays the code before doing countdown work and countup tracking work
         //Added this if condition again to catch the 0 second anomaly
         //condition added for display of correctness and else clause functions just normally
         if(secondHand === 60){
-            document.getElementById("timeDisplay").innerText = (minuteHand+1) +"min"+00+"seconds" +newLine+timeBlockArray[counter];
+            //printing by label
+            document.getElementById("min").innerText = (minuteHand+1);
+            document.getElementById("sec").innerText = padZero(defaultSecond);      
         }else{
-            document.getElementById("timeDisplay").innerText = (minuteHand) +"min"+(secondHand)+"seconds" +newLine+timeBlockArray[counter];
+            
+            document.getElementById("forsec").innerText = padZero(timeBlockArray[counter]);
+            
+            //printing by label
+            document.getElementById("min").innerText = padZero(minuteHand);
+            document.getElementById("sec").innerText = padZero(secondHand);            
         }
+
+        //condition to check for changing colour
+        //maybe can use array.includes here
+        if(secondHand ===0 ||secondHand ===30 || secondHand ===60) {
+            document.getElementById("thirtysecondtimedisplay").style.backgroundColor = 'yellow';
+        }else if(secondHand === 10 || secondHand ===40){
+            document.getElementById("thirtysecondtimedisplay").style.backgroundColor = 'red';
+        }
+
         currentCount =timeBlockArray[counter];
         timeBlockArray[counter]--;
         secondHand--;
         minutePassed++;
+        
         console.log(minutePassed);
         
     }
@@ -186,13 +217,17 @@ function tabathaCount(){
 }
 
 function stopTime(){
-    timeBlockArray[counter]= currentCount;
+    
+    currentCount = timeBlockArray[counter];
     isClicked = false;
+    
     clearInterval(startTimeCounter);
+    clearInterval(startMilliTimeCounter);
 }
 
 function resetTime(){
     isClicked = false;
+    minutePassed = 0;
     document.getElementById("minutelabel").value=defaultMinute;
     document.getElementById("secondlabel").value=defaultSecond;
     
@@ -201,7 +236,25 @@ function resetTime(){
     calculateBlocksOfTime();
     
     clearInterval(startTimeCounter);
+    clearInterval(startMilliTimeCounter);
 
-    document.getElementById("timeDisplay").innerText = (minuteTimeOverall) +"min"+ (defaultSecond) +" seconds" +newLine+timeBlockArray[counter];
+    //For the total time display
+    document.getElementById("min").innerText = padZero(defaultMinute);
+    document.getElementById("sec").innerText = padZero(defaultSecond);
+    document.getElementById("mili").innerText = padZero(defaultMilisecond);
+
+    //For the interval Counter
+    document.getElementById("formin").innerText = padZero(defaultSecond);
+    document.getElementById("forsec").innerText = padZero(defaultIntervalCounter);
+    document.getElementById("formili").innerText = padZero(defaultMilisecond);
+
+}
+
+
+//https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript/10073788
+//padding zero in front
+function padZero(number){
+    let  str = number.toString();
+    return str.padStart(2, "0");
 }
 
